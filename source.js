834a9924139a8f2fadcac7426d02d75889db5f91
shubh-2012 <77185813+shubh-2012@users.noreply.github.com>
2024-04-20 15:48:42 +0530

    creation of index file for browser and source.js source.js file is imported in index.html file source.js include the function creation in js normal way and with => function

---
+++ source.js
@@ -0,0 +1,26 @@
+
+
+// functions
+
+function sum(a,b){
+    return a + b;
+}
+
+const output1 = sum();
+
+console.log(output1);
+
+// arrow function
+
+const name1 = () => 'hello shubham';   
+
+/** for arrow function the function keyword is not required and to return function in 
+one line no return statement is required but with multi line code for fucntion { return ...} is required .
+parameters can be passed in () */
+
+const sum2 = (a,b) => {
+    return a + b;
+}
+
+console.log(sum2());
+
