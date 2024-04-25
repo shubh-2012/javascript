a15239d09146da4ac0ef34e726094b3ed6fc9eee
shubh-2012 <77185813+shubh-2012@users.noreply.github.com>
2024-04-25 10:00:38 +0530

    findIndex function

--- source.js
+++ source.js
@@ -269,7 +269,61 @@ console.log(neg);
   console.log(outputsong);
 
 
-// end filter function
+// end filter function  
+
+// find product in stock
+
+  const productList = [
+    { id: "p1", name: "Apples", category: "Fruits", price: 2.5, inStock: true },
+    {
+      id: "p2",
+      name: "Oranges",
+      category: "Fruits",
+      price: 1.99,
+      inStock: false
+    },
+    {
+      id: "p3",
+      name: "Blueberries",
+      category: "Fruits",
+      price: 4.99,
+      inStock: true
+    },
+    {
+      id: "p4",
+      name: "Carrots",
+      category: "Vegetables",
+      price: 1.0,
+      inStock: true
+    },
+    {
+      id: "p5",
+      name: "Tomatoes",
+      category: "Vegetables",
+      price: 1.5,
+      inStock: true
+    }
+  ];
+  function searchAndBuyProduct(productList,name){
+    //Implement Your function here.
+     let prodsale = false;
+     const productIndex = productList.findIndex((currentProd)=>currentProd.name === name);
+
+     if ( productIndex != -1 ){
+      if ( productList[productIndex]['inStock'] == true){
+        productList[productIndex]['inStock'] = false;
+        prodsale = true;
+      }
+     }
+
+      return prodsale;
+      
+  }
+
+  const output = searchAndBuyProduct(productList,"Tomatoes");
+  console.log(output);
+
+// end find product in stock
 
 
 
