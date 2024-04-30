e939eda7e2676ccdd530c30a506c9c7b94941fdb
shubh-2012 <77185813+shubh-2012@users.noreply.github.com>
2024-05-01 00:20:40 +0530

    seat booking app V0

---
+++ Seat_booking app/index.js
@@ -0,0 +1,8 @@
+const moviesList = [
+    { movieName: "Flash", price: 7 },
+    { movieName: "Spiderman", price: 5 },
+    { movieName: "Batman", price: 4 },
+  ];
+
+  const vacantSeat = document.getElementsByClassName('row');
+
