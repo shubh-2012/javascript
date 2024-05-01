ab85de2a421cc79106123053f63281870f368204
shubh-2012 <77185813+shubh-2012@users.noreply.github.com>
2024-05-01 13:24:32 +0530

    seat booking app v2

--- Seat_booking app/index.js
+++ Seat_booking app/index.js
@@ -10,12 +10,52 @@ const moviesList = [
 
   for (let i = 0; i<vacantSeat.length; i++){
     vacantSeat[i].addEventListener('click',(event)=>{
+      const arraySeatClass = vacantSeat[i].classList.contains('occupied');
+      if(arraySeatClass == false){
         vacantSeat[i].classList.toggle('selected');
         const seatSelectedTemp = document.getElementsByClassName('selected');
         const seatCount = seatSelectedTemp.length;
         const seatCountShow = document.getElementById('numberOfSeat');
         seatCountShow.innerHTML = seatCount - 1;
-    });
+        let movieSelected = document.getElementById('movieName').innerHTML;
+        const chosenMovie1 = moviesList.find(movie=>movie.movieName == movieSelected);
+        document.getElementById('totalPrice').innerHTML = `$ ${chosenMovie1.price * (seatCount-1)}`;
+      }
+        });
   }
 
   //   end of : add event listener to all seats to toggle colour when selected/unselected.
+
+//   start of : add movies from available list to select your movie dropdown
+// display ticket price for the same
+
+    const movieDropdown = document.getElementById('selectMovie');
+
+    moviesList.forEach((current)=>{
+        
+        let option = document.createElement('option');
+
+        option.text = current.movieName;
+
+        movieDropdown.add(option);
+    });
+
+    
+    
+    movieDropdown.addEventListener('change',(eve)=>{
+        let selectedMovie = movieDropdown.options[movieDropdown.selectedIndex].text;
+        document.getElementById('movieName').innerHTML = selectedMovie;
+        const chosenMovie = moviesList.find(movie=>movie.movieName == selectedMovie);
+        document.getElementById('moviePrice').innerHTML = `$ ${chosenMovie.price}`;
+        const seatSelectedTemp = document.getElementsByClassName('selected');
+        const seatCount = seatSelectedTemp.length;
+        document.getElementById('totalPrice').innerHTML = `$ ${chosenMovie.price * (seatCount-1)}`;
+    });
+
+//   end of : add movies from available list to select your movie dropdown
+
+// get total price
+
+    
+
+// end of : get total price
