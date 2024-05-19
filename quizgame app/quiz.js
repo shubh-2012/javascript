4f4080e8537059521bcae5132e03fb3f6ab38e52
shubh-2012 <77185813+shubh-2012@users.noreply.github.com>
2024-05-19 17:43:31 +0530

    quiz app with shuffled options

--- quizgame app/quiz.js
+++ quizgame app/quiz.js
@@ -1,36 +1,42 @@
-const questionObj = 
-    {
-      category: 'Food & Drink',
-      id: 'qa-1',
-      correctAnswer: 'Three ',
-      options: ['Two', 'Three ', 'Four', 'Five'],
-      question:
-        "How many pieces of bun are in a Mcdonald's Big Mac?",
-    };
-  
-    const{correctAnswer, options, question}=questionObj;
+const questionObj = {
+  category: "Food & Drink",
+  id: "qa-1",
+  correctAnswer: "Three ",
+  options: ["Two", "Three ", "Four", "Five"],
+  question: "How many pieces of bun are in a Mcdonald's Big Mac?",
+};
 
-    const questionEl = document.getElementById("question");
-    const optionEl = document.getElementById("options");
-    const scoreEl = document.getElementById("score");
+const { correctAnswer, options, question } = questionObj;
 
-    questionEl.textContent = question;
-    let score = 0;
+const questionEl = document.getElementById("question");
+const optionEl = document.getElementById("options");
+const scoreEl = document.getElementById("score");
 
-    options.forEach((option)=>{
-        const newButton = document.createElement("button");
-        newButton.textContent = option;
-        optionEl.appendChild(newButton);
-        newButton.addEventListener('click',()=>{
-          if(newButton.textContent === correctAnswer){
-            score++;
-          }else{
-            score = score - 0.25
-          }
-          scoreEl.textContent =  `score:${score}`;
-          questionEl.textContent = 'Quiz Completed';
-          optionEl.textContent = '';
-        });
-    });
+questionEl.textContent = question;
+let score = 0;
 
+let shuffledOptionsArray = shuffledOptions(options);
 
+shuffledOptionsArray.forEach((option) => {
+  const newButton = document.createElement("button");
+  newButton.textContent = option;
+  optionEl.appendChild(newButton);
+  newButton.addEventListener("click", () => {
+    if (newButton.textContent === correctAnswer) {
+      score++;
+    } else {
+      score = score - 0.25;
+    }
+    scoreEl.textContent = `score:${score}`;
+    questionEl.textContent = "Quiz Completed";
+    optionEl.textContent = "";
+  });
+});
+
+function shuffledOptions(options) {
+  for (let i = options.length - 1; i >= 0; i--) {
+    const j = Math.floor(Math.random() * i + 1);
+    [options[i], options[j]] = [options[j], options[i]];
+  }
+  return options;
+}
