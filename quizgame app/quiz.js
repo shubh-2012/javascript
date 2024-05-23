ecd854381d06ef7c35ed69292bd55ddea3b35a57
shubh-2012 <77185813+shubh-2012@users.noreply.github.com>
2024-05-23 09:44:07 +0530

    quiz game final 1.0

--- quizgame app/quiz.js
+++ quizgame app/quiz.js
@@ -1,36 +1,85 @@
-const questionObj = {
-  category: "Food & Drink",
-  id: "qa-1",
-  correctAnswer: "Three ",
-  options: ["Two", "Three ", "Four", "Five"],
-  question: "How many pieces of bun are in a Mcdonald's Big Mac?",
-};
+const questionJSON = [
+  {
+    category: "Food & Drink",
+    id: "qa-1",
+    correctAnswer: "Three ",
+    options: ["Two", "Three ", "Four", "Five"],
+    question: "How many pieces of bun are in a Mcdonald's Big Mac1?",
+  },
+  {
+    category: "Food & Drink",
+    id: "qa-2",
+    correctAnswer: "Three ",
+    options: ["Two", "Three ", "Four", "Five"],
+    question: "How many pieces of bun are in a Mcdonald's Big Mac2?",
+  },
+  {
+    category: "Food & Drink",
+    id: "qa-3",
+    correctAnswer: "Three ",
+    options: ["Two", "Three ", "Four", "Five"],
+    question: "How many pieces of bun are in a Mcdonald's Big Mac3?",
+  },
+  {
+    category: "Food & Drink",
+    id: "qa-4",
+    correctAnswer: "Three ",
+    options: ["Two", "Three ", "Four", "Five"],
+    question: "How many pieces of bun are in a Mcdonald's Big Mac4?",
+  },
+  {
+    category: "Food & Drink",
+    id: "qa-5",
+    correctAnswer: "Three ",
+    options: ["Two", "Three ", "Four", "Five"],
+    question: "How many pieces of bun are in a Mcdonald's Big Mac5?",
+  },
+];
 
-const { correctAnswer, options, question } = questionObj;
+// const questionObj = {
+//   category: "Food & Drink",
+//   id: "qa-1",
+//   correctAnswer: "Three ",
+//   options: ["Two", "Three ", "Four", "Five"],
+//   question: "How many pieces of bun are in a Mcdonald's Big Mac?",
+// };
+
+// const { correctAnswer, options, question } = questionObj;
+
+let score = 0;
+let currentQuestion = 0;
 
 const questionEl = document.getElementById("question");
 const optionEl = document.getElementById("options");
 const scoreEl = document.getElementById("score");
+const nextEl = document.getElementById("next");
 
-questionEl.textContent = question;
-let score = 0;
+function showQuestion() {
+  const { correctAnswer, options, question } = questionJSON[currentQuestion];
+  questionEl.textContent = question;
 
-let shuffledOptionsArray = shuffledOptions(options);
+  let shuffledOptionsArray = shuffledOptions(options);
 
-shuffledOptionsArray.forEach((option) => {
-  const newButton = document.createElement("button");
-  newButton.textContent = option;
-  optionEl.appendChild(newButton);
-  newButton.addEventListener("click", () => {
-    if (newButton.textContent === correctAnswer) {
-      score++;
-    } else {
-      score = score - 0.25;
-    }
-    scoreEl.textContent = `score:${score}`;
-    questionEl.textContent = "Quiz Completed";
-    optionEl.textContent = "";
+  shuffledOptionsArray.forEach((option) => {
+    const newButton = document.createElement("button");
+    newButton.textContent = option;
+    optionEl.appendChild(newButton);
+    newButton.addEventListener("click", () => {
+      if (newButton.textContent === correctAnswer) {
+        score++;
+      } else {
+        score = score - 0.25;
+      }
+      scoreEl.textContent = `score:${score}`;
+      nextQuestion();
+    });
   });
+}
+
+showQuestion();
+
+nextEl.addEventListener("click", () => {
+  nextQuestion();
 });
 
 function shuffledOptions(options) {
@@ -39,4 +88,15 @@ function shuffledOptions(options) {
     [options[i], options[j]] = [options[j], options[i]];
   }
   return options;
+}
+
+function nextQuestion() {
+  currentQuestion++;
+  optionEl.textContent = "";
+  if (currentQuestion >= questionJSON.length) {
+    questionEl.textContent = "Quiz Completed";
+    nextEl.remove();
+  } else {
+    showQuestion();
+  }
 }
